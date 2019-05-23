3ea1cbf9cc10480aa7ee50e6961252abf5d62bc4
Alex Merecka <merecka@gmail.com>
2019-05-23 04:08:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -87,17 +87,16 @@ class Student
     end.first
   end
 
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(input)
     sql = <<-SQL
     SELECT *
     FROM students
-    WHERE grade = 10
-    LIMIT 1
+    WHERE grade = #{input}
     SQL
 
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first
+    end
   end
 
   def save
